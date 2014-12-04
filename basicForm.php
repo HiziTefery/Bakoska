bbd1fe9500370411ae2d57589bb6ee6f7a351573
Lubos Hyzak <hizitefery@gmail.com>
2014-12-04 13:34:55 +0100

    Complete design-functional on mobile phones.

--- basicForm.php
+++ basicForm.php
@@ -13,13 +13,13 @@ try
     $rowsFound = $data->rowCount();
     if($rowsFound > 0) {
 	echo "
-	    <div class='table-container'><table id=data_table><tr><th>ID</th><th>temperature</th><th>barometric pressure</th><th>humidity</th><th>timestamp of measurement</th></tr>";
+	    <div class='table-container'><div id='overfl'><table id=data_table><tr><th>ID</th><th>temperature</th><th>barometric pressure</th><th>humidity</th><th>timestamp of measurement</th></tr>";
 	// output data of each row
     foreach($data as $row) 
     {	
 	    echo "<tr><td>".$row["id"]."</td><td>".$row["temperature"]."</td><td>".$row["barometric_pressure"]."</td><td>".$row["humidity"]."</td><td>".$row["timestamp_of_measurement"]."</td></tr>";
 	}
-	echo "</table></div>";
+	echo "</table></div></div>";
 
     }
     else {
